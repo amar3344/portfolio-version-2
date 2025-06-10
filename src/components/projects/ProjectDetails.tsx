b32db7bed691fa6b948840f1e33fe270acf7f73d
Amar kumar <amarkumarreddynew@gmail.com>
2025-06-10 22:14:26 +0530

    update the project details

--- src/components/projects/ProjectDetails.tsx
+++ src/components/projects/ProjectDetails.tsx
@@ -1,87 +1,89 @@
 import React from 'react';
 import { Modal } from "react-bootstrap";
 import AwesomeSlider from "react-awesome-slider";
+// import AwesomeSliderStyles from "./light-Slider.scss";
+// import AwesomeSliderStyles2 from "./dark-Slider.scss";
 import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
 import 'react-awesome-slider/dist/styles.css';
 import "../../index.css"
 
-interface IProps{
-    onHide:()=>void;
-    show:boolean,
-    data:any
+interface IProps {
+  onHide: () => void;
+  show: boolean,
+  data: any
 }
 
-export default function ProjectDetails(props:IProps) {
+export default function ProjectDetails(props: IProps) {
+  console.log("DATA IMGE", props.data.images)
 
-    const tech = (icons:any,i:any) => {
-        return (
-            <li className="list-inline-item mx-3" key={i}>
-                <span>
-                    <div className="text-center">
-                        <i className={icons.class} style={{ fontSize: "300%" }}>
-                            <p className="text-center" style={{ fontSize: "30%" }}>
-                                {icons.name}
-                            </p>
-                        </i>
-                    </div>
-                </span>
-            </li>
-        );
-    }
-  return (
-    <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-        className="modal-inside"
-      >
-        <span onClick={props.onHide} className="modal-close">
-          <i className="fas fa-times fa-3x close-icon"></i>
-        </span>
-        <div className="col-md-12">
-          <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
-            <div className="slider-tab">
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="emojione:red-circle"
-                data-inline="false"
-                style={{ marginLeft: "5px" }}
-              ></span>{" "}
-              &nbsp;{" "}
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:yellow-circle"
-                data-inline="false"
-              ></span>{" "}
-              &nbsp;{" "}
-              <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:green-circle"
-                data-inline="false"
-              ></span>
-            </div>
-          </div>
-          <div className="col-md-10 mx-auto">
-            <h3 style={{ padding: "5px 5px 0 5px" }}>
-              {props.data.map((eachTitle:{title:string}) => eachTitle.title)}
-              <a
-                  href={props.data.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="link-href"
-                >
-                  <i
-                    className="fas fa-external-link-alt"
-                    style={{ marginLeft: "10px" }}
-                  ></i>
-                </a>
-            </h3>
-            <p className="modal-description">{props.data.map((eachItem:{description:string}) => eachItem.description)}</p>
-            <div className="col-md-12 text-center">
-            </div>
-          </div>
+  const renderImageContainer = () => {
+    if (props.data.images) {
+      return (
+        <AwesomeSlider
+          // cssModule={[AwesomeSliderStyles, AwesomeSliderStyles2]}
+          animation="scaleOutAnimation"
+          className="slider-image"
+        >
+          {props.data.images.map((elem:string,i:number)=> <div key={i} data-src={elem} />)}
+        </AwesomeSlider>
+      )
+  }
+}
+return (
+  <Modal
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    className="modal-inside"
+  >
+    <span onClick={props.onHide} className="modal-close">
+      <i className="fas fa-times fa-3x close-icon"></i>
+    </span>
+    <div className="col-md-12">
+      <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
+        <div className="slider-tab">
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="emojione:red-circle"
+            data-inline="false"
+            style={{ marginLeft: "5px" }}
+          ></span>{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="twemoji:yellow-circle"
+            data-inline="false"
+          ></span>{" "}
+          &nbsp;{" "}
+          <span
+            className="iconify slider-iconfiy"
+            data-icon="twemoji:green-circle"
+            data-inline="false"
+          ></span>
         </div>
-      </Modal>
-  )
+      </div>
+      {renderImageContainer()}
+      <div className="col-md-10 mx-auto">
+        <h3 style={{ padding: "5px 5px 0 5px" }}>
+          {props.data.title}
+          <a
+            href={props.data.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-href"
+          >
+            <i
+              className="fas fa-external-link-alt"
+              style={{ marginLeft: "10px" }}
+            ></i>
+          </a>
+        </h3>
+        <p className="modal-description">{props.data.description}</p>
+        <div className="col-md-12 text-center">
+        </div>
+      </div>
+    </div>
+  </Modal>
+)
 }
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,39 @@
 import "../../App.css"
 import "../../index.css"
+import ProjectDetailsModal from "./ProjectDetails";
+import { useState } from "react"
 
 
 interface IProps {
     resumeBasicInfo: any
 }
 
+const img1 = require("./lessericon.webp")
+const img2 = require("./lesser1.jpeg")
+const img3 = require("./lesser2.jpeg")
+
 const projects =  [
+     {
+        "title": "Lesser",
+        "startDate": "2023",
+        "description": "Lesser is an application directed to help recyclers in Saudi to find the nearest recycling station and dispose of their waste. While doing so, the recyclers will get points that allow them to redeem in exchange for discount codes or helpful products.",
+        "images": [
+            img1,
+            img2,
+            img3
+        ],
+        "url": "https://play.google.com/store/apps/details?id=com.LESSER2&hl=en_IN&pli=1",
+        "technologies": [
+            {
+                "class": "devicon-react-plain",
+                "name": "ReactNative"
+            },
+            {
+                "class": "devicon-typescript-plain",
+                "name": "TypeScript"
+            },
+        ]
+    },
     {
         "title": "OffSpace",
         "startDate": "2025",
@@ -79,12 +106,16 @@ const projects =  [
 
 export default function Projects(props: IProps) {
 
+    const [detailsModalShow,setdetailsModalShow] = useState(false)
+    const [currentProject,setCurrentProject]  = useState({})
+
     const currentProjects = (projects:{title:string,images:string[],startDate:string}) =>{
          return (
                 <div
                     className="col-sm-12 col-md-6 col-lg-4"
                     key={projects.title}
                     style={{ cursor: "pointer" }}
+                    onClick={()=>handleDetailsModal(projects)}
                 >
                     <span className="portfolio-item d-block">
                         <div className="foto">
@@ -106,6 +137,15 @@ export default function Projects(props: IProps) {
                 </div>
             );
     }
+
+    const handleDetailsModal = (project:any) =>{
+        setCurrentProject(project)
+        setdetailsModalShow(true)
+    }
+    const closeDetailsModal = () =>{
+        setdetailsModalShow(false)
+
+    }
     return (
         <section id="portfolio">
             <div className="col-md-12">
@@ -117,6 +157,11 @@ export default function Projects(props: IProps) {
                         projects.map((eachProject:any)=>currentProjects(eachProject))
                         }</div>
                 </div>
+                <ProjectDetailsModal
+                    show={detailsModalShow}
+                    onHide={closeDetailsModal}
+                    data={currentProject}
+                />
             </div>
         </section>
     )
